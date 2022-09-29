38502cae9685902c362c4a43f7688b91c821beca
JP Thomas <jp.thomas@piperaccess.com>
2022-09-29 10:36:59 -0600

    get first recipe info from list of recipes

--- main.py
+++ main.py
@@ -19,26 +19,28 @@ units = {}
 units['g'] = 1
 units['kg'] = 1000
 
-@dataclass
+
 class Nutrition:
 	"""Class for keeping track of nutrition info"""
 
-@dataclass
+
 class Ingredient:
 	"""Class for keeping track of an ingredient"""
 	name: str
 	amount: float = 0
 
-@dataclass
+
 class RecipeStep:
 	"""Class for keeping track of recipe steps"""
 	description: str
 	time: float #min
 
 
-@dataclass
+
 class Recipe:
 	"""Class for keeping track of a recipe"""
+	name: str
+	description: str
 	ingredients: list[Ingredient]
 	servings: float
 	author: str
@@ -48,7 +50,21 @@ class Recipe:
 	storageTimeLimit: float #days
 	steps: list[RecipeStep]
 	notes: str
+	source: str
 
+def printRecipe(recipe, depth='summary'):
+	"""
+	prints recipe data to the console
+	
+	depth : how much data should be printed
+			'summary' = name, author, prepTime, cookTime
+	"""
+	if(depth=='summary'):
+		print(recipe.name, 'by: ', recipe.author, 'prep: ', recipe.prepTime, 'cook: ', recipe.cookTime)
+	elif(depth=='ingredients'):
+		print(recipe.ingredients)
+	else:
+		print(recipe)
 
 
 # start with scraping a recipe and testing stuff out a bit, vegan minimalist baker of course
@@ -63,7 +79,7 @@ if(page.status_code != 200):
 
 soup = bs(page.text, 'html.parser')
 
-articles = soup.find_all('article')
+articles = soup.find_all('article', class_='post-summary')
 articleLinks = []
 for article in articles:
 	link = article.a.get('href')
@@ -77,6 +93,12 @@ testIngredientData = Ingredient()
 testRecipeStep = RecipeStep()
 testRecipeData = Recipe()
 #wprm-recipe-container-97936 > div > div.wprm-recipe-header-container > div.wprm-recipe-header-left > h2
-soupRecipe.find('div', class_='wprm-recipe')
+recipeDiv = soupRecipe.find('div', class_='wprm-recipe')
+testRecipeData.name = recipeDiv.select_one('.wprm-recipe-name').text
+testRecipeData.description = recipeDiv.select_one('.wprm-recipe-summary').text
+testRecipeData.author = recipeDiv.select_one('.wprm-recipe-author').text
+testRecipeData.prepTime = recipeDiv.select_one('.wprm-recipe-prep_time').text
+testRecipeData.cookTime = recipeDiv.select_one('.wprm-recipe-cook_time').text
 
+printRecipe(testRecipeData)
 # print(soup.prettify())
