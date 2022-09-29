575e6d7daf94667cb0b32694a71ea8d204ba9122
JP Thomas <jp.thomas@piperaccess.com>
2022-09-29 11:11:36 -0600

    add rest of recipe metadata

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from dataclasses import dataclass
 recipes = {}
 
 
-# need to keep track of unit mapping
+# need to keep track of unit mapping, maybe use pint: https://pint.readthedocs.io/en/stable/
 units = {}
 units['g'] = 1
 units['kg'] = 1000
@@ -41,16 +41,18 @@ class Recipe:
 	"""Class for keeping track of a recipe"""
 	name: str
 	description: str
-	ingredients: list[Ingredient]
+	ingredients: list[Ingredient] = []
 	servings: float
+	servingsUnit: str
 	author: str
 	prepTime: float #minutes
 	cookTime: float #minutes
 	freezerFriendly: bool
 	storageTimeLimit: float #days
-	steps: list[RecipeStep]
+	steps: list[RecipeStep] = []
 	notes: str
 	source: str
+	tags: list[str] = []
 
 def printRecipe(recipe, depth='summary'):
 	"""
@@ -64,7 +66,8 @@ def printRecipe(recipe, depth='summary'):
 	elif(depth=='ingredients'):
 		print(recipe.ingredients)
 	else:
-		print(recipe)
+		for attr, value in recipe.__dict__.items():
+			print(attr, value)
 
 
 # start with scraping a recipe and testing stuff out a bit, vegan minimalist baker of course
@@ -99,6 +102,12 @@ testRecipeData.description = recipeDiv.select_one('.wprm-recipe-summary').text
 testRecipeData.author = recipeDiv.select_one('.wprm-recipe-author').text
 testRecipeData.prepTime = recipeDiv.select_one('.wprm-recipe-prep_time').text
 testRecipeData.cookTime = recipeDiv.select_one('.wprm-recipe-cook_time').text
+testRecipeData.servings = recipeDiv.select_one('.wprm-recipe-servings').text
+testRecipeData.servingsUnit = recipeDiv.select_one('.wprm-recipe-servings-unit').text
+testRecipeData.tags += [_.strip() for _ in recipeDiv.select_one('.wprm-recipe-course').text.split(',')]
+testRecipeData.tags += [_.strip() for _ in recipeDiv.select_one('.wprm-recipe-cuisine').text.split(',')]
+testRecipeData.freezerFriendly = recipeDiv.select_one('.wprm-recipe-freezer-friendly').text
+testRecipeData.storageTimeLimit = recipeDiv.select_one('.wprm-recipe-does-it-keep').text
 
-printRecipe(testRecipeData)
+printRecipe(testRecipeData, depth='all')
 # print(soup.prettify())
