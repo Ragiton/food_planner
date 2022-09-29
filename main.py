b022f0249588929adb6981bb56621b69dfb988a2
JP Thomas <jp.thomas@piperaccess.com>
2022-09-28 22:05:30 -0600

    pulling first recipe from site

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 
+from email.mime import base
 import string
+from tracemalloc import start
 from bs4 import BeautifulSoup as bs
 import requests
 import re
@@ -16,7 +18,6 @@ recipes = {}
 units = {}
 units['g'] = 1
 units['kg'] = 1000
-units['']
 
 @dataclass
 class Nutrition:
@@ -50,3 +51,32 @@ class Recipe:
 
 
 
+# start with scraping a recipe and testing stuff out a bit, vegan minimalist baker of course
+
+baseurl = 'https://minimalistbaker.com/'
+starturl = 'recipe-index/?fwp_special-diet=vegan'
+
+page = requests.get(baseurl + starturl)
+
+if(page.status_code != 200):
+	raise RuntimeError('Failed to load page')
+
+soup = bs(page.text, 'html.parser')
+
+articles = soup.find_all('article')
+articleLinks = []
+for article in articles:
+	link = article.a.get('href')
+	print(link)
+	articleLinks.append(link)
+
+pageRecipe = requests.get(articleLinks[0])
+soupRecipe = bs(pageRecipe.text, 'html.parser')
+
+testIngredientData = Ingredient()
+testRecipeStep = RecipeStep()
+testRecipeData = Recipe()
+#wprm-recipe-container-97936 > div > div.wprm-recipe-header-container > div.wprm-recipe-header-left > h2
+soupRecipe.find('div', class_='wprm-recipe')
+
+# print(soup.prettify())
