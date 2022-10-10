6ac673f49bbc9664c69bd97c49a68749e210d27d
JP Thomas <jp.thomas@piperaccess.com>
2022-10-10 17:06:06 -0600

    move getRecipeData into function

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 
-
-from email.mime import base
-import string
-from tracemalloc import start
 from bs4 import BeautifulSoup as bs
 import requests
 import requests_cache
 import re
+import sitemaps
 
 from dataclasses import dataclass, field
 
@@ -61,7 +58,8 @@ class Recipe:
 	storageTimeLimit: float  = None#days
 	steps: list[RecipeStep] = field(default_factory=list)
 	notes: str = None
-	source: str = None
+	sourceLink: str = None
+	printLink: str = None
 	tags: list[str] = field(default_factory=list)
 
 def printRecipe(recipe, depth='summary'):
@@ -80,62 +78,74 @@ def printRecipe(recipe, depth='summary'):
 			if attr not in ['ingredients']:
 				print(attr,':', value)
 		print('Ingredients:')
-		if ingredients is not None and ingredients is not []:
-			for i in ingredients:
+		if recipe.ingredients is not None and recipe.ingredients is not []:
+			for i in recipe.ingredients:
 				print('\t', i)
 
+def getRecipeFromPage(recipeUrl):
+	"""
+	Get recipe info from WPRM (word press recipe maker) site
+	"""
+	recipePage = requests.get(recipeUrl)
+	recipeSoup = bs(recipePage.text, 'html.parser').find('div', class_='wprm-recipe')
+
+	testIngredientData = Ingredient()
+	recipeData = Recipe()
+	recipeData.sourceLink = recipeUrl
+
+	recipeData.printLink = printTag = recipeSoup.find('a', class_='wprm-recipe-print', href=True)['href']
+	#wprm-recipe-container-97936 > div > div.wprm-recipe-header-container > div.wprm-recipe-header-left > h2
+	recipeData.name = recipeSoup.select_one('.wprm-recipe-name').text
+	recipeData.description = recipeSoup.select_one('.wprm-recipe-summary').text
+	recipeData.author = recipeSoup.select_one('.wprm-recipe-author').text
+	recipeData.prepTime = recipeSoup.select_one('.wprm-recipe-prep_time').text
+	recipeData.cookTime = recipeSoup.select_one('.wprm-recipe-cook_time').text
+	recipeData.servings = recipeSoup.select_one('.wprm-recipe-servings').text
+	recipeData.servingsUnit = recipeSoup.select_one('.wprm-recipe-servings-unit').text
+	recipeData.tags += [_.strip() for _ in recipeSoup.select_one('.wprm-recipe-course').text.split(',')]
+	recipeData.tags += [_.strip() for _ in recipeSoup.select_one('.wprm-recipe-cuisine').text.split(',')]
+	recipeData.freezerFriendly = recipeSoup.select_one('.wprm-recipe-freezer-friendly').text
+	recipeData.storageTimeLimit = recipeSoup.select_one('.wprm-recipe-does-it-keep').text
+	recipeData.notes = recipeSoup.select_one('.wprm-recipe-notes').text
+
+	# get ingredients
+	ingredients = []
+	ingredientSoup = recipeSoup.find('div', class_='wprm-recipe-ingredient-group')
+	for ingredientItem in ingredientSoup.find_all('li', class_='wprm-recipe-ingredient'):
+		amount = ingredientItem.find('span', class_='wprm-recipe-ingredient-amount').text
+		unit = ingredientItem.find('span', class_='wprm-recipe-ingredient-unit').text
+		name = ingredientItem.find('span', class_='wprm-recipe-ingredient-name').text
+		notes = ingredientItem.find('span', class_='wprm-recipe-ingredient-notes')
+		if notes is not None:
+			notes = notes.text
+		ingredients.append(Ingredient(name, amount, unit, notes))
+
+	recipeData.ingredients = ingredients
+	printRecipe(recipeData, depth='all')
+	return recipeData
 
 # start with scraping a recipe and testing stuff out a bit, vegan minimalist baker of course
 
-baseurl = 'https://minimalistbaker.com/'
-starturl = 'recipe-index/?fwp_special-diet=vegan'
+def scrapeRecipeLinks():
+	baseurl = 'https://minimalistbaker.com/'
+	starturl = 'recipe-index/?fwp_special-diet=vegan'
 
-page = requests.get(baseurl + starturl)
+	page = requests.get(baseurl + starturl)
 
-if(page.status_code != 200):
-	raise RuntimeError('Failed to load page')
+	if(page.status_code != 200):
+		raise RuntimeError('Failed to load page')
 
-soup = bs(page.text, 'html.parser')
+	soup = bs(page.text, 'html.parser')
 
-articles = soup.find_all('article', class_='post-summary')
-articleLinks = []
-for article in articles:
-	link = article.a.get('href')
-	print(link)
-	articleLinks.append(link)
+	articles = soup.find_all('article', class_='post-summary')
+	articleLinks = []
+	for article in articles:
+		link = article.a.get('href')
+		print(link)
+		articleLinks.append(link)
+	
+	return articleLinks
 
-pageRecipe = requests.get(articleLinks[0])
-soupRecipe = bs(pageRecipe.text, 'html.parser')
+articleLinks = scrapeRecipeLinks()
 
-testIngredientData = Ingredient()
-testRecipeStep = RecipeStep()
-testRecipeData = Recipe()
-#wprm-recipe-container-97936 > div > div.wprm-recipe-header-container > div.wprm-recipe-header-left > h2
-recipeSoup = soupRecipe.find('div', class_='wprm-recipe')
-testRecipeData.name = recipeSoup.select_one('.wprm-recipe-name').text
-testRecipeData.description = recipeSoup.select_one('.wprm-recipe-summary').text
-testRecipeData.author = recipeSoup.select_one('.wprm-recipe-author').text
-testRecipeData.prepTime = recipeSoup.select_one('.wprm-recipe-prep_time').text
-testRecipeData.cookTime = recipeSoup.select_one('.wprm-recipe-cook_time').text
-testRecipeData.servings = recipeSoup.select_one('.wprm-recipe-servings').text
-testRecipeData.servingsUnit = recipeSoup.select_one('.wprm-recipe-servings-unit').text
-testRecipeData.tags += [_.strip() for _ in recipeSoup.select_one('.wprm-recipe-course').text.split(',')]
-testRecipeData.tags += [_.strip() for _ in recipeSoup.select_one('.wprm-recipe-cuisine').text.split(',')]
-testRecipeData.freezerFriendly = recipeSoup.select_one('.wprm-recipe-freezer-friendly').text
-testRecipeData.storageTimeLimit = recipeSoup.select_one('.wprm-recipe-does-it-keep').text
-
-# get ingredients
-ingredients = []
-ingredientSoup = recipeSoup.find('div', class_='wprm-recipe-ingredient-group')
-for ingredientItem in ingredientSoup.find_all('li', class_='wprm-recipe-ingredient'):
-	amount = ingredientItem.find('span', class_='wprm-recipe-ingredient-amount').text
-	unit = ingredientItem.find('span', class_='wprm-recipe-ingredient-unit').text
-	name = ingredientItem.find('span', class_='wprm-recipe-ingredient-name').text
-	notes = ingredientItem.find('span', class_='wprm-recipe-ingredient-notes')
-	if notes is not None:
-		notes = notes.text
-	ingredients.append(Ingredient(name, amount, unit, notes))
-
-testRecipeData.ingredients = ingredients
-printRecipe(testRecipeData, depth='all')
-# print(soup.prettify())
+getRecipeFromPage(articleLinks[0])
